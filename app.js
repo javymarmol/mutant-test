1ac9df6a6e872c2c9ae0aae01bceeca8f06638c2
javymarmol <contacto@javymarmol.com>
2021-03-01 01:18:10 -0500

    add documentation

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 const routerMutant = require('./routes/mutant');
 const routerStats = require('./routes/stats');
+const swaggerDoc = require('./swagger.json');
 
 const app = express();
 
@@ -8,11 +10,12 @@ app.use(express.json());
 
 // app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.status(200).send('It is working! 🤘');
-});
+// app.get('/', (req, res) => {
+//   res.status(200).send('It is working! 🤘');
+// });
 
 app.use('/mutant', routerMutant);
 app.use('/stats', routerStats);
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 module.exports = app;
